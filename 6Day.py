8cc436bf2125d89359f580c9689fe1785765c477
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-08-25 21:27:58 +0300

    6Day in week2

---
+++ 6Day.py
@@ -0,0 +1,22 @@
+#convert to int
+num = int(1)
+num2 = int(4.9)
+print("INT")
+print(num)
+print(num2)
+
+#convert to float
+x = float(4)
+y = float(6.7)
+print("FLOAT")
+print(x)
+print(y)
+
+#convert to string
+a = str(2)
+b = str(3.3)
+z = str("AR")
+print("STRONH")
+print(a)
+print(b)
+print(z)
