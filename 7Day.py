50775c96f65326a799917d1f3884a2f3b9207ae5
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-08-26 18:29:58 +0300

    7Day

---
+++ 7Day.py
@@ -0,0 +1,14 @@
+name = "Arwa"
+print(name)
+
+# Multiline Strings
+x = """Hello I'm very excited to finish 
+learning progrmming in python """
+print(x)
+
+name2 = "Arwa ali "
+print(name2[3])
+print(name2[2])
+print(name2[5])
+print(name2[5:7])
+print(name2[0:3])
