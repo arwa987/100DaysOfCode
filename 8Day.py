c9cd59c3b9181fc09c80f65036e2e67e108f2fbd
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-08-27 05:54:48 +0300

    8Day

---
+++ 8Day.py
@@ -0,0 +1,23 @@
+name = " Arwa, Ali "
+
+#The strip() method
+print(name.strip())
+
+#The len() method
+print(len(name))
+
+#The lower() method
+print(name.lower())
+
+#The upper() method
+print(name.upper())
+
+#The replace() method
+print(name.replace("w","W"))
+
+#The split() method
+print(name.split(","))
+
+x = "hi Asma"
+print(x.split(" "))
+print(x.split("A"))
