68c3b1cad24c095b4fd24f0eee63a120ab715b9c
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-08-29 19:02:26 +0300

    10Day

---
+++ 10Day.py
@@ -0,0 +1,23 @@
+#Arithmetic Operators
+num1 = 9
+num2 = 3
+num3 =8
+print(num1*num2)
+print(num1+num2)
+print(num1%num2)
+
+#Assignment Operators
+num3+=1
+num1/=3
+num2**=2
+print(num3)
+print(num1)
+print(num2)
+
+#Comparison Operators
+x =9
+y =6
+z =9
+print(x==7)
+print(x > y)
+print(x!=z)
