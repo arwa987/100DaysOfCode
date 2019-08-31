baebb6305334c3d4d3f92664ff3b2be97135b390
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-08-31 03:19:38 +0300

    11Day

---
+++ 11Day.py
@@ -0,0 +1,14 @@
+num = 9
+print(num>7 and  num<10)
+
+x = "arwa"
+y = "ali"
+z = y
+# return false becuase z is same object as y
+print(z is not y )
+# return false becuase x is not same object as y
+print(x is y)
+
+name = "apple , orange"
+print("apple" in name)
+print("A" in name)
