18c9f7b200ceefda91bd0ad96cd805a61bdc1aa5
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-09-02 17:20:07 +0300

    14Day

---
+++ 14Day.py
@@ -0,0 +1,19 @@
+list = [2.3 ,3.2 , 8.4, 8.7, 5.9, 5.6]
+print("The list1 is :")
+print(list)
+print("The items from index 0 to index 5 is:")
+print(list[0:5])
+
+#Check if number 8.7 is present in the list.
+if 8.7 in list:
+    print("Yes 8.7 is in numbers list")
+
+
+#Repeat Item in List
+print("The list2 is :")
+list2 =[7]*5
+print(list2)
+
+list3 =list + list2
+print("The list3 is :")
+print(list3)
