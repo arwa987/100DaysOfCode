7d6e652bbf3c29b3c48dd150a851b9ca96518543
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-09-05 01:11:00 +0300

    16Day

---
+++ 16Day.py
@@ -0,0 +1,24 @@
+#create tuple
+tuple1 = ("A","B","C","D",123, 6, 7)
+print(tuple1)
+
+tuple2 =("E",)
+print(tuple2)
+
+#Access Tuple Items
+print("The items in index 2 is : ")
+print(tuple1[2])
+
+#print for all items in tuple
+print("All items in tuple is ")
+for a in tuple1:
+    print(a)
+
+#
+print("print items in tuple from index 0 to index 4 :")
+print(tuple1[0:4])
+
+"""delete the tuple
+del tuple2
+print(tuple2)
+"""
