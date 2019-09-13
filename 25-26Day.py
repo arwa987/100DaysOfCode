f320dce87e9a05652219e3fd4bba7961000c107e
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-09-13 13:56:26 +0300

    last day in week4

---
+++ 25-26Day.py
@@ -0,0 +1,21 @@
+set ={1,3,5,7,8}
+set.update([4,8,12])
+print(set)
+
+set.remove(8)
+print(set)
+
+set.clear()
+print(set)
+
+x ={
+    "name":"pigeon",
+    "type":"brid",
+    "skin cover":"feathers"
+}
+print(x)
+y =x["type"]
+print(y)
+
+x["skin cover"]="anything"
+print(x)
