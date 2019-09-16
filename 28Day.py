36e4fffbd0098c97197ef952367b6c11dbf6dbc4
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-09-16 23:43:42 +0300

    Add files via upload

---
+++ 28Day.py
@@ -0,0 +1,28 @@
+a = 0
+while a < 7 :
+    print(a)
+    a +=1
+
+print("---------------------E2-----------------------------")
+b =1
+while b < 6 :
+    print(b)
+    if b ==4:
+      break
+    b += 1
+
+print("---------------------E3-----------------------------")
+b2 =0
+while b2 < 6 :
+    b2 += 1
+    if b2 == 5:
+        continue
+    print(b2)
+print("---------------------E4-----------------------------")
+b3 =1
+while b3 < 10:
+    print(b3)
+    b3 += 1
+else:
+    print("b3 is no longer less than 10")
+
