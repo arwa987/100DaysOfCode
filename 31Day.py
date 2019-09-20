f678560580cea77f06b9fc72f8e721183f5efeb1
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-09-20 16:38:15 +0300

    31Day

---
+++ 31Day.py
@@ -0,0 +1,21 @@
+def my_function():
+    print("Hi,my name is arwa")
+
+my_function()
+
+print("---------------My Funcation2----------------")
+def my_funcation2(name):
+    print("Hi "+name)
+
+my_funcation2("arwa")
+my_funcation2("sara")
+my_funcation2("asma")
+
+print("---------------My Funcation3----------------")
+def my_funcation3(ID=000):
+    print("My ID is ",ID)
+
+my_funcation3(123)
+my_funcation3(555)
+my_funcation3(765)
+my_funcation3()
