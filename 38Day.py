f2cade085199099276cb962d9babe36c30a42b96
arwa987 <53653243+arwa987@users.noreply.github.com>
2019-09-26 23:06:55 +0300

    38Day

---
+++ 38Day.py
@@ -0,0 +1,13 @@
+list = ["A","B","C"]
+
+for x in list:
+    print(x)
+
+#Adding Array Elements
+list.append("D")
+print(list)
+#Removing Array Elements
+list.pop(1)
+print(list)
+list.remove("D")
+print(list)
